c42740a60202555ad8d80cfc93f06cf9c801a489
Matthew Saxe <mattsaxe17@gmail.com>
2021-10-23 13:03:38 -0400

    Add authentication

--- api/routes.js
+++ api/routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const controllers = require('./controllers');
+const auth = require('./middleware').auth;
 
 // EXAMPLES (Each route has a controller)
-router.get('/example', controllers.getExample);
+router.get('/example', auth, controllers.getExample); // Use the auth middleware to authorize requests. Requests must have a valid JWT token from the auth service
 router.post('/example', controllers.setExample);
 
 module.exports = router;
