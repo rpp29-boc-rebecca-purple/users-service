073d747a9c24c382eb72720fe9770a056dc7e969
Aleksandar Cakic <acakic92@gmail.com>
2021-10-29 17:29:35 -0500

    Define Routes for Users Service

--- api/routes.js
+++ api/routes.js
@@ -1,9 +1,28 @@
 const router = require('express').Router();
 const controllers = require('./controllers');
-const auth = require('./middleware').auth;
 
-// EXAMPLES (Each route has a controller)
-router.get('/example', auth, controllers.getExample); // Use the auth middleware to authorize requests. Requests must have a valid JWT token from the auth service
-router.post('/example', controllers.setExample);
+/// USER ROUTES ///
+
+// GET request to render user profile information based on provided user ID
+router.get('/user/:userId', controllers.getProfile);
+
+// PUT request to update the user profile (provided userID must match currently logged in user)
+router.put('/user/:userId/edit', controllers.putEditProfile);
+
+/// FRIEND ROUTES ///
+
+// GET request for rendering a list of all friends for the current user
+// -> returns an array of objects containing information for each friend of current user
+router.get('/user/:userId/friendsList', controllers.getFriendsList);
+
+// POST request to follow a new friend
+router.post('/user/:userId/friendsList/:fid/follow', controllers.postFriendFollow);
+
+// PUT request to unfollow a friend
+router.put('/user/:userId/friendsList/:fid/unfollow', controllers.putFriendFollow);
+
+// GET request to search for new friends based on provided email address
+router.get('/searchFriend/:email', controllers.getSearchFriends);
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const routes = require('./api/routes');
 require('dotenv').config();
 
 const app = express();
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
